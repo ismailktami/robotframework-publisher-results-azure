9959398207c43b0c86a8ea72e255e3ed886a426a
Ismail Ktami <ismail.ktami@cdbdx.biz>
2022-01-27 02:01:12 +0100

    fix issue suites results

--- PublisherAzureTestPlan/SuiteResults.py
+++ PublisherAzureTestPlan/SuiteResults.py
@@ -9,7 +9,8 @@ class SuiteResults(ResultVisitor):
         self.suites = []
 
     def start_suite(self, suite):
-        self.get_testcases_id_status(suite)
+        if len(suite.suites) == 0:
+            self.get_testcases_id_status(suite)
 
     def get_testcases_id_status(self, suite):
         tests_ids_status = {}
@@ -17,39 +18,37 @@ class SuiteResults(ResultVisitor):
         global testplanId, suiteId
         testplanId = suiteId = None
         testcases = {}
-        for s in suite.suites:
-            if len(s.tests) > 0:
-                for tc in s.tests:
-                    infoTestCase = {}
-                    try:
-                        infoTestCase['id'] = SuiteResults.get_tags_with_prefixe(
-                            tc, Constants.PREFIXE_TAG_TEST_ID)
-                        infoTestCase['starttime'] = tc.starttime
-                        infoTestCase['endtime'] = tc.endtime
-                        infoTestCase['elapsedtime'] = tc.elapsedtime
-                        infoTestCase['critical'] = tc.critical
-                        infoTestCase['message'] = tc.message
-                        infoTestCase['name'] = tc.name
-                        infoTestCase["status"] = tc.status
-                        infoTestCase["planId"] = SuiteResults.get_tags_with_prefixe(
-                            tc, Constants.PREFIXE_TAG_TESTPLAN_ID)
-                        infoTestCase["suiteId"] = SuiteResults.get_tags_with_prefixe(
-                            tc, Constants.PREFIXE_TAG_SUITE_ID)
-                        tests_ids_status[infoTestCase['id']
-                                         ] = infoTestCase['status']
-                        testcases[infoTestCase['id']] = infoTestCase
-                        suiteId = SuiteResults.get_tags_with_prefixe(
-                            tc, 'SUITE-')
-                        testplanId = SuiteResults.get_tags_with_prefixe(
-                            tc, 'TESTPLAN-')
-                    except Exception as ex:
-                        tests_witout_ids.append({"name": tc.name})
-                        pass
-                if len(tests_witout_ids):
-                    logger.warning(
-                        "\nSutie "+s.name+" ==> Tests without IDs:\n"+str(tests_witout_ids))
-                self.suites.append({'name': s.name, 'id': suiteId, 'planId': testplanId, 'tests_ids_status': tests_ids_status,
-                                   'tests_witout_ids': tests_witout_ids, 'testcases': testcases, 'starttime': suite.starttime, 'endtime': suite.endtime})
+        for tc in suite.tests:
+            infoTestCase = {}
+            try:
+                infoTestCase['id'] = SuiteResults.get_tags_with_prefixe(
+                    tc, Constants.PREFIXE_TAG_TEST_ID)
+                infoTestCase['starttime'] = tc.starttime
+                infoTestCase['endtime'] = tc.endtime
+                infoTestCase['elapsedtime'] = tc.elapsedtime
+                infoTestCase['critical'] = tc.critical
+                infoTestCase['message'] = tc.message
+                infoTestCase['name'] = tc.name
+                infoTestCase["status"] = tc.status
+                infoTestCase["planId"] = SuiteResults.get_tags_with_prefixe(
+                    tc, Constants.PREFIXE_TAG_TESTPLAN_ID)
+                infoTestCase["suiteId"] = SuiteResults.get_tags_with_prefixe(
+                    tc, Constants.PREFIXE_TAG_SUITE_ID)
+                tests_ids_status[infoTestCase['id']
+                                 ] = infoTestCase['status']
+                testcases[infoTestCase['id']] = infoTestCase
+                suiteId = SuiteResults.get_tags_with_prefixe(
+                    tc, 'SUITE-')
+                testplanId = SuiteResults.get_tags_with_prefixe(
+                    tc, 'TESTPLAN-')
+            except Exception as ex:
+                tests_witout_ids.append({"name": tc.name})
+                pass
+        if len(tests_witout_ids):
+            logger.warning(
+                "\nSutie "+suite.name+" ==> Tests without IDs:\n"+str(tests_witout_ids))
+        self.suites.append({'name': suite.name, 'id': suiteId, 'planId': testplanId, 'tests_ids_status': tests_ids_status,
+                            'tests_witout_ids': tests_witout_ids, 'testcases': testcases, 'starttime': suite.starttime, 'endtime': suite.endtime})
 
     def get_testcase_ids(self, suite):
         print(suite['tests_ids_status'])
